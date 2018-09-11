66283eb68a02e37d4bcd899beebe4e02f0dd5528
Thomas <thmiv4@gmail.com>
2018-09-11 15:06:13 -0500

    9111506

--- models/Author.js
+++ models/Author.js
@@ -1,3 +1,7 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function (sequelize, DataTypes) {
   var Author = sequelize.define("Author", {
     username: {
@@ -18,6 +22,16 @@ module.exports = function (sequelize, DataTypes) {
       onDelete: "cascade"
     });
   };
+
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  Author.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  Author.hook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
 
   return Author;
 };
