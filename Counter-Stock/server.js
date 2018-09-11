dcd2f01bf16312986209696dab12ded14ec399f9
mostar <mominahmad93@gmail.com>
2018-09-11 11:50:04 -0500

    added percent change and totalvalue

--- Counter-Stock/server.js
+++ Counter-Stock/server.js
@@ -26,13 +26,13 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true }; // reminder: set back to fasle
+var syncOptions = { force: false }; // reminder: set back to fasle
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
