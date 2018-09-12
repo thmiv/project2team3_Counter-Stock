63d59eaf995533d3d7366e9c9e672cc297740510
thmiv <thmiv4@gmail.com>
2018-09-12 00:23:57 -0500

    sort-of working signup page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,15 +5,15 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
   // AUTH ROUTES****************************************************
 
-/*
-  app.get("/", function(req, res) {
+
+  app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup");
   });
-
+/*
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
