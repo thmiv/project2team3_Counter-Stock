48fe9ccf1720c09dd463bfdb14a625ef2940478e
thmiv <thmiv4@gmail.com>
2018-09-12 18:53:21 -0500

    fixed script loading order

--- public/js/createchar.js
+++ public/js/createchar.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     $.get("/api/userdata").then(function (data) {
-        $(".member-name").text(data.username + " " + data.password);
+        $(".member-name").text(data.username);
     });
 });
 
